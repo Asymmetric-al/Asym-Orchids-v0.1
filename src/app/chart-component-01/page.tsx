7340bca36c1538a1aa20fd5f45f2efdf843c4ab6
Asymmetric-al <conrad@globalfellowship.org>
2025-12-15 23:19:53 +0700

    Auto-sync from Orchids Desktop

---
+++ src/app/chart-component-01/page.tsx
@@ -0,0 +1,15 @@
+import MissionaryMetricsCard from '@/components/shadcn-studio/blocks/chart-missionary-metrics'
+
+const MissionaryMetricsPreview = () => {
+  return (
+    <div className='py-8 sm:py-16 lg:py-24'>
+      <div className='mx-auto max-w-7xl px-4 sm:px-6 lg:px-8'>
+        <div className='flex justify-center'>
+          <MissionaryMetricsCard className='w-full max-w-200' />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default MissionaryMetricsPreview
